24ba77ce53ab46f099b6983995c195cfb86ccd81
BalramApply <balramapply123@gmail.com>
2026-06-17 08:17:45 +0530

    sixteenth day

--- Expression_Type.java
+++ Expression_Type.java
@@ -1,7 +1,7 @@
 /* ┌─────────────────────── DSA ATTEMPTS ─────────────────────────────────┐
  * │  #1  [04/06/26] → #2  [05/06/26] → #3  [06/06/26]                    │
  * │  #4  [07/06/26] → #5  [08/06/26] → #6  [09/06/26]                    │
- * │  #7  [00/06/26] → #8  [00/06/26] → #9  [00/06/26]                    │
+ * │  #7  [10/06/26] → #8  [00/06/26] → #9  [00/06/26]                    │
  * │  #10 [00/06/26]                                                      │
  * └──────────────────────────────────────────────────────────────────────┘
  *
@@ -107,17 +107,21 @@ public class Expression_Type {
  * └─────────────────────────────────────────────────────────────────────┘
  */
 
-// public class Expression_Type {
-//     public static void main(String args[]) {
-//         byte b = 4;
-//         char c = 'a';   // ASCII value of 'a' is 97
-//         short s = 512;
-//         int i = 1000;
-//         float f = 3.14f;
-//         double d = 99.9954;
-
-//         // Calculation breakdown
-//         double result = (f * b) + (i % c) - (d * s);
-//         System.out.println("RESULT OF THE EXPRESSION IS : "+result); // Expected Output : 
-//     }
-// }
+/*╔══════════════════════════════════════════════════════════════════════╗
+* ║ public class Expression_Type {                                       ║
+* ║     public static void main(String args[]) {                         ║
+* ║         byte b = 4;                                                  ║
+* ║         char c = 'a';   // ASCII value of 'a' is 97                  ║
+* ║         short s = 512;                                               ║
+* ║         int i = 1000;                                                ║
+* ║         float f = 3.14f;                                             ║
+* ║         double d = 99.9954;                                          ║
+* ║                                                                      ║
+* ║         // Calculation breakdown                                     ║
+* ║         double result = (f * b) + (i % c) - (d * s);                 ║
+* ║         System.out.println("RESULT OF THE EXPRESSION IS : "+result); ║
+* ║         // Expected Output : -51154.0856                             ║
+* ║     }                                                                ║
+* ║ }                                                                    ║
+* ╚══════════════════════════════════════════════════════════════════════╝
+*/
