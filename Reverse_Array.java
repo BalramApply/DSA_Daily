e09c672e4c6280ffe355e80a357854b2d4f3c2f7
BalramApply <balramapply123@gmail.com>
2026-07-16 22:17:45 +0530

    Thirty_Six_Day

--- Reverse_Array.java
+++ Reverse_Array.java
@@ -58,29 +58,10 @@
  */
 
 public class Reverse_Array {
-    public static void reverse(int numbers[]) {
-        int first = 0;
-        int last = numbers.length - 1;
-
-        while (first < last) {
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
-            first++;
-            last--;
-        }
-    }
-
-    public static void print(int numbers[]) {
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-    }
 
     public static void main(String[] args) {
         int numbers[] = { 1, 2, 3, 4, 5 };
-
-    }
+    }   
 }
 
 /* ┌─────────────────────── DSA ATTEMPTS ─────────────────────────────────┐
